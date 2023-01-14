48bc2d8de3d85419bffe28602a0dc94e45e4872e
limengxu <1354786446@qq.com>
2023-01-14 17:59:51 +0800

    配置router

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { memo } from 'react'
-
-const App = memo(() => {
-  return (
-    <div>Hello World</div>
-  )
-})
-
-export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import { HashRouter } from 'react-router-dom';
 
+import App from '@/App';
 import 'normalize.css'
 import './assets/css/index.less'
 
@@ -13,6 +14,10 @@ import './assets/css/index.less'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback="loading">
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Suspense>
   </React.StrictMode>
 );
