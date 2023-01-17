2a4673954ea5cd9f20b87461bc1bb8dd31160041
limengxu <1354786446@qq.com>
2023-01-17 10:35:58 +0800

    配置redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from '@/App';
+import store from './store';
 import 'normalize.css'
 import './assets/css/index.less'
 
@@ -15,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Suspense fallback="loading">
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <Provider store={store}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </Provider>
     </Suspense>
   </React.StrictMode>
 );
---
+++ src/store/modules/entire/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  count: 9
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default reducer
