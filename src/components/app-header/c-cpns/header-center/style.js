da505964b3cb1b01ad5fcc70dbcfbdaa53a5c7ad
limengxu <1354786446@qq.com>
2023-01-29 11:03:33 +0800

    使用svg配置logo

--- src/components/app-header/c-cpns/header-center/style.js
+++ src/components/app-header/c-cpns/header-center/style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const CenterWrapper = styled.div`
-  
+
 `
--- src/components/app-header/c-cpns/header-left/index.jsx
+++ src/components/app-header/c-cpns/header-left/index.jsx
@@ -1,9 +1,12 @@
+import IconLogo from '@/assets/svg/icon_logo'
 import React, { memo } from 'react'
 import { LeftWrapper } from './style'
 
 const HeaderLeft = memo(() => {
   return (
-    <LeftWrapper>HeaderLeft</LeftWrapper>
+    <LeftWrapper>
+      <IconLogo/>
+    </LeftWrapper>
   )
 })
 
