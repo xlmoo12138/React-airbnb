fbf69b97cb90011cc6d48387cbc03cb3ef5d30d6
limengxu <1354786446@qq.com>
2023-01-31 00:19:59 +0800

    实现footer和房间item的封装

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,3 +1,4 @@
+import RoomItem from '@/components/room-item'
 import SectionHeader from '@/components/section-header'
 import { fetchHomeDataAction } from '@/store/modules/home'
 import React, { memo, useEffect } from 'react'
@@ -22,7 +23,14 @@ const Home = memo(() => {
       <HomeBanner />
       <div className="content">
         <div className="good-price">
-          <SectionHeader title={ goodPriceInfo.title } />
+          <SectionHeader title={goodPriceInfo.title} />
+          <ul className='room-list'>
+            {
+              goodPriceInfo.list?.slice(0, 8)?.map(item => {
+                return <RoomItem itemData={ item } key={ item.id } />
+              })
+            }
+          </ul>
         </div>
       </div>
     </HomeWrapper>
