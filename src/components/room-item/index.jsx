749652507472fd321cdc498e26e68c883e4a7e01
limengxu <1354786446@qq.com>
2023-01-31 12:15:13 +0800

    实现房间item的布局

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -5,8 +5,17 @@ import { ItemWrapper } from './style'
 const RoomItem = memo((props) => {
   const { itemData } = props
   return (
-    <ItemWrapper>
-      {itemData.name}
+    <ItemWrapper verifyColor={itemData?.verify_info?.text_color || "#39576a"}>
+      <div className="inner">
+        <div className="cover">
+          <img src={itemData.picture_url} alt="" />
+        </div>
+        <div className="desc">
+          {itemData.verify_info.messages.join("·")}
+        </div>
+        <div className="name">{itemData.name}</div>
+        <div className="price">¥{itemData.price}/晚</div>
+      </div>
     </ItemWrapper>
   )
 })
