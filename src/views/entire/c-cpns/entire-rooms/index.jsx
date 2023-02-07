23f3ad8beff99b0c91048e5f2621f4ef5cc180bb
limengxu <1354786446@qq.com>
2023-02-07 18:46:17 +0800

    全部页面过滤条件展示和选中

---
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+import { RoomsWrapper } from './style'
+
+const EntireRooms = memo((props) => {
+  return (
+    <RoomsWrapper>EntireRooms</RoomsWrapper>
+  )
+})
+
+
+export default EntireRooms
