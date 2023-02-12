0d7d547b6a5e2870990238ce80a377e8aaa27f53
limengxu <1354786446@qq.com>
2023-02-13 00:29:53 +0800

    全部-封装Indicator组件--特殊情况处理

--- src/base-ui/indicator/index.jsx
+++ src/base-ui/indicator/index.jsx
@@ -14,12 +14,21 @@ const Indicator = memo((props) => {
 
     // 2. 获取content的宽度
     const contentWidth = contentRef.current.clientWidth
+    const contentScroll = contentRef.current.scrollWidth
+
     // 3. 获取selectIndex要滚动的距离
     // item的offsetLeft + item的width * 0.5 - indicator的width * 0.5
-    const distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5
+    let distance = itemLeft + itemWidth * 0.5 - contentWidth * 0.5
 
+    // 4. 特殊情况的处理
+    if (distance < 0) distance = 0 // 左边的特殊情况处理
+    const totalDistance = contentScroll - contentWidth
+    if (distance > totalDistance) distance = totalDistance // 右边的特殊情况处理
+
+    // 5. 改变位置即可
     contentRef.current.style.transform = `translate(${-distance}px)`
   }, [selectIndex])
+  
   return (
     <IndicatorWrapper>
       <div className="i-content" ref={contentRef}>
